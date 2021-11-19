cae30fd1320bcae93748dc4e7225c6f24470824c
jrachucki <jrachucki@n7space.com>
2021-11-19 09:28:34 +0100

    Example fixed

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -13,9 +13,6 @@ static constexpr int SEND_THREAD_STACK_SIZE1 = 65536;
 static constexpr int SEND_THREAD_PRIORITY2 = 1;
 static constexpr int SEND_THREAD_STACK_SIZE2 = 65536;
 
-linux_serial_ccsds_private_data serial1{};
-linux_serial_ccsds_private_data serial2{};
-
 void* bus_to_driver_private_data[16];
 void* bus_to_driver_send_function[16];
 void* interface_to_deliver_function[16];
@@ -28,6 +25,9 @@ main()
 {
     printf("\n\rDemo app started\n\r");
 
+    linux_serial_ccsds_private_data serial1{};
+    linux_serial_ccsds_private_data serial2{};
+
     taste::Thread sendThread1{ SEND_THREAD_PRIORITY1, SEND_THREAD_STACK_SIZE1 };
     taste::Thread sendThread2{ SEND_THREAD_PRIORITY2, SEND_THREAD_STACK_SIZE2 };
 
@@ -37,13 +37,13 @@ main()
     serial1.driver_init(BUS_INVALID_ID, DEVICE_INVALID_ID, &device1, nullptr);
     serial2.driver_init(BUS_INVALID_ID, DEVICE_INVALID_ID, &device2, nullptr);
 
-    int someargs1;
-    // int someargs2;
-    sendThread1.start(&sendThreadMethod1, &someargs1);
-    // sendThread2.start(&sendThreadMethod2, &someargs2);
+    linux_serial_ccsds_private_data* serials[2] = { &serial1, &serial2 };
+
+    sendThread1.start(&sendThreadMethod1, serials);
+    sendThread2.start(&sendThreadMethod2, &serial2);
 
     sendThread1.join();
-    // sendThread2.join();
+    sendThread2.join();
 
     printf("\n\rDemo app finished");
     return 0;
@@ -52,23 +52,23 @@ main()
 void
 sendThreadMethod1(void* args)
 {
-    (int*)args;
+    linux_serial_ccsds_private_data** serial;
+    serial = (linux_serial_ccsds_private_data**)args;
     static constexpr int numOfExe{ 10 };
     for(uint16_t i = 0; i < numOfExe; i++) {
-        serial1.driver_send(TEXT1, sizeof(TEXT1));
-        serial2.driver_send(TEXT2, sizeof(TEXT2));
-        usleep(500000);
+        serial[0]->driver_send(TEXT1, sizeof(TEXT1));
+        usleep(250000);
     }
 }
 
 void
 sendThreadMethod2(void* args)
 {
-    (int*)args;
+    linux_serial_ccsds_private_data* serial;
+    serial = (linux_serial_ccsds_private_data*)args;
     static constexpr int numOfExe{ 20 };
     for(uint16_t i = 0; i < numOfExe; i++) {
-        printf("\n\rSender2");
-        serial2.driver_send(TEXT2, sizeof(TEXT2));
+        serial->driver_send(TEXT2, sizeof(TEXT2));
         usleep(250000);
     }
 }
